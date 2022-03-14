981a0ff750df4d8d750d4d27e4b06ebc87079400
huthaifaTayeh <huthaifazmi@gmail.com>
2022-03-14 23:58:23 +0300

    fixed ESLINT violations

--- huthaifa/components/imageUpload/ImageUpload.js
+++ huthaifa/components/imageUpload/ImageUpload.js
@@ -72,7 +72,7 @@ export default function ImageUpload({ imageSetter }, props) {
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="45px"
-								class="h-6 w-6"
+								className="h-6 w-6"
 								fill="none"
 								viewBox="0 0 24 24"
 								stroke="darkgrey"
@@ -84,7 +84,7 @@ export default function ImageUpload({ imageSetter }, props) {
 								/>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
-									class="h-5 w-5"
+									className="h-5 w-5"
 									viewBox="-27 13 60 20"
 									width="30px"
 									fill="black">
